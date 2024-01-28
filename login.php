e0436864a09f0086dbb75e416f5b7c160111acfd
ViniciosGabriel1 <viniciosking309@gmail.com>
2024-01-28 12:06:19 -0300

    First Commit

---
+++ login.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/login.css">
+    <title>login</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="container-login">
+            <div class="wrapper-login">
+                <form action="processa_login.php" class="login-form">
+                    <span class="form-title">Faça Login</span>
+
+                    <div class="wrap-input margin-top-35 margin-bottom-35">
+                        <input type="email" name="email" class="input-form">
+                        <span class="focus-input-form" data-placeholder="E-mail"></span>
+                    </div>
+
+                    <div class="wrap-input margin-bottom-35">
+                        <input type="password" name="password" class="input-form">
+                        <span class="focus-input-form" data-placeholder="Senha"></span>
+                    </div>
+
+                    <div id="container-login-form-btn">
+                        <button class="login-form-btn">Entrar</button><br>
+                    </div>
+
+                    <ul>
+                        <li class="margin-top-8 margin-bottom-8">
+                            <a href="#" class="text1">Esqueceu a senha?</a><br>
+                        </li>
+
+                        <li class="margin-top-8 margin-bottom-8">
+                            <a href="#" class="text2">Não tem conta?</a>
+                        </li>
+                    </ul>
+                </form>
+            </div>
+            <img src="img/doutor.svg" alt="Login" width="500" height="500">
+        </div>
+        <script>
+            let inputs = document.getElementsByClassName('input-form');
+
+            for (let input of inputs) {
+                input.addEventListener("input", function() {
+                    if (input.value.trim() !== "") {
+                        input.classList.add("has-val");
+                    } else {
+                        input.classList.remove("has-val");
+                    }
+                });
+            }
+        </script>
+
+    </div>
+
+</body>
+
+</html>
