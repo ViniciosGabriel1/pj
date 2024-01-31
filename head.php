040be043c1f9256c9dfcf0c9a4cef4e44ad1f1cb
ViniciosGabriel1 <viniciosking309@gmail.com>
2024-01-31 16:34:53 -0300

    atualização responsividade

---
+++ head.php
@@ -0,0 +1,13 @@
+<header class="header">
+    <div class="logo">
+        <img src="img/logo.png" alt="Logo da empresa">
+    </div>
+    <nav class="menu">
+        <ul>
+            <li><a href="#">Hmoe</a></li>
+            <li><a href="#">Sobre</a></li>
+            <li><a href="#">Serviços</a></li>
+            <li><a href="#">Contato</a></li>
+        </ul>
+    </nav>
+</header>
--- index.php
+++ index.php
@@ -6,25 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/global.css">
     <link rel="stylesheet" href="css/index.css">
-    <title>Menu à Direita e Logo à Esquerda</title>
+    <title>Página Inicial</title>
 </head>
 
 <body>
-
-    <header class="header">
-        <div class="logo">
-            <img src="img/logo.png" alt="Logo da empresa">
-        </div>
-        <nav class="menu">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Sobre</a></li>
-                <li><a href="#">Serviços</a></li>
-                <li><a href="#">Contato</a></li>
-            </ul>
-        </nav>
-    </header>
-
+<?php
+include "head.php";
+?>
     <h1>Qual a boa de Hoje?</h1>
 
     <section class="profissionais">
@@ -49,7 +37,37 @@
         </div>
 
         <div class="profissional-card">
+            <img src="img/medico2.avif" alt="Foto do Profissional">
+            <div class="profissional-info">
+                <h3>Dr. Show</h3>
+                <p>Especialidade: Clínico Geral</p>
+                <p>Experiência: Mais de 10 anos</p>
+                <button class="botao">Escolher</button>
+            </div>
+        </div>
+
+        <div class="profissional-card">
             <img src="img/medico.webp" alt="Foto do Profissional">
+            <div class="profissional-info">
+                <h3>Dr. Show</h3>
+                <p>Especialidade: Clínico Geral</p>
+                <p>Experiência: Mais de 10 anos</p>
+                <button class="botao">Escolher</button>
+            </div>
+        </div>
+
+        <div class="profissional-card">
+            <img src="img/medico2.avif" alt="Foto do Profissional">
+            <div class="profissional-info">
+                <h3>Dr. Show</h3>
+                <p>Especialidade: Clínico Geral</p>
+                <p>Experiência: Mais de 10 anos</p>
+                <button class="botao">Escolher</button>
+            </div>
+        </div>
+
+        <div class="profissional-card">
+            <img src="img/medico2.avif" alt="Foto do Profissional">
             <div class="profissional-info">
                 <h3>Dr. Show</h3>
                 <p>Especialidade: Clínico Geral</p>
